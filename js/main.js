1a3d86a4653705fdfd551737390410c0c4d76368
Nelson Modro Jr <jrmodro@gmail.com>
2018-07-19 21:46:46 -0300

    starting js

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// para declarar variáveis usar let ou const
+
+/*
+// select case é switch(){case}
+
+let numRodas = 5;
+switch(numRodas){
+    case 1:
+    case 2:
+    case 3:
+        console.log('Valor do pedágio: R$ 1,50');
+        break;
+    case 4:
+        console.log('Valor do pedágio: R$ 4,50');
+        break;
+    default:
+        console.log('Valor do pedágio: R$ ', numRodas * 1.75);
+}
+*/
+
+/*
+let contador1 = 12;
+let contador2 = contador1;
+while(contador1 <=10){
+    console.log('while ', contador1);
+    contador1++;
+}
+do{
+    console.log('do ', contador2);
+    contador2++;
+} while(contador2 <= 10)
+*/
