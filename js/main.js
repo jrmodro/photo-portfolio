b610794178c5cb67d2a76f80232177b30b7e7936
Nelson Modro Jr <jrmodro@gmail.com>
2018-07-21 16:50:57 -0300

    open close button

--- js/main.js
+++ js/main.js
@@ -1,32 +1,10 @@
-// para declarar variáveis usar let ou const
+// usando a API do HTLM DOM
 
-/*
-// select case é switch(){case}
+let botao = document.querySelector('.menu-principal__btn');
+let menuPrincipal = document.querySelector('.menu-principal');
 
-let numRodas = 5;
-switch(numRodas){
-    case 1:
-    case 2:
-    case 3:
-        console.log('Valor do pedágio: R$ 1,50');
-        break;
-    case 4:
-        console.log('Valor do pedágio: R$ 4,50');
-        break;
-    default:
-        console.log('Valor do pedágio: R$ ', numRodas * 1.75);
+botao.addEventListener("click", abreFechaMenu);
+
+function abreFechaMenu(evento){
+    menuPrincipal.classList.toggle('menu-principal--fechado');
 }
-*/
-
-/*
-let contador1 = 12;
-let contador2 = contador1;
-while(contador1 <=10){
-    console.log('while ', contador1);
-    contador1++;
-}
-do{
-    console.log('do ', contador2);
-    contador2++;
-} while(contador2 <= 10)
-*/
